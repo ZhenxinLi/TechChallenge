8303b0c64c3fd8e0ed723bba93c2d2b2df18c27a
Zhenxin Li <jackielizhenxin@gmail.com>
2022-08-01 07:42:17 +1000

    modified script

--- ansible/scripts/run-ansible.sh
+++ ansible/scripts/run-ansible.sh
@@ -8,4 +8,4 @@ echo "$(cd .. && cat .env | grep -oP "(?<=(DB\=)).*")" >> inventory.yml
 
 export ANSIBLE_HOST_KEY_CHECKING=False
 
-ansible-playbook -v -i ../inventory.yml -e 'record_host_keys=True' -u ec2-user ../playbook.yml -e "db_url=$(cd .. && cat .env | grep -oP "(?<=(DB\=)).*")"
+ansible-playbook -v -i inventory.yml -e 'record_host_keys=True' -u ec2-user ../playbook.yml -e "db_url=$(cd .. && cat .env | grep -oP "(?<=(DB\=)).*")"
