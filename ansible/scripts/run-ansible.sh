10f300037598a1ea63dfd92c4540ea5293c7e0d9
Zhenxin Li <jackielizhenxin@gmail.com>
2022-08-04 09:18:32 +1000

    final modification of ansible

--- ansible/scripts/run-ansible.sh
+++ ansible/scripts/run-ansible.sh
@@ -3,9 +3,8 @@
 
 echo "[web]" >> inventory.yml
 echo "$(cd .. && cat .env | grep -oP "(?<=(WEB\=)).*")" >> inventory.yml
-echo "[db]" >> inventory.yml
-echo "$(cd .. && cat .env | grep -oP "(?<=(DB\=)).*")" >> inventory.yml
 
 export ANSIBLE_HOST_KEY_CHECKING=False
 
-ansible-playbook -v -i inventory.yml -e 'record_host_keys=True' -u ec2-user ../playbook.yml -e "db_url=$(cd .. && cat .env | grep -oP "(?<=(DB\=)).*")"
+ansible-playbook -v -i inventory.yml -e 'record_host_keys=True' -u ec2-user --extra-vars "db_endpoint=$(cd ../../infra && terraform output db_endpoint) db_user=$(cd ../../infra && terraform output db_user) db_pass=$(cd ../../infra && terraform output db_password) lb_endpoint=$(cd ../../infra && terraform output lb_endpoint)" ../playbook.yml
+
---
+++ ansible/templates/service.tpl
@@ -0,0 +1,7 @@
+"DbUser" = "{{ db_user }}"
+"DbPassword" = "{{ db_password }}"
+"DbName" = "{{ db_name }}"
+"DbPort" = "{{ db_port }}"
+"DbHost" = "{{ actual_db_endpoint }}"
+"ListenHost" = "{{ listen_host }}"
+"ListenPort" = "{{ listen_port }}"
